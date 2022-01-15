adf21cc0c64828a9e94ddb95d0e3ff5f17702d74
Cristian Nita <cristiannita24@gmail.com>
2022-01-15 14:26:59 +0200

    New Spectrum Visualization

--- src/visuals/spectrum.js
+++ src/visuals/spectrum.js
@@ -1,30 +1,32 @@
 function Spectrum() {
-	this.name = "spectrum";
+	this.name = "Spectrum";
 
 	this.draw = function () {
 		push();
-		var spectrum = fourier.analyze();
+
+		let spectrum = fourier.analyze();
 		noStroke();
-		//fill(0,255,0)
-		// for (var i = 0; i< spectrum.length; i++){
-		// 	var x = map(i, 0, spectrum.length, 0, width);
-		//     var h = -height + map(spectrum[i], 0, 255, height, 0);
-		//     rect(x, height, width / spectrum.length, h );
-		// 		}
 
+		for (let i = 0; i < spectrum.length; i += 5) {
 
-		for (var i = 0; i < spectrum.length; i++) {
-
-			//fade the colour of the bin from green to red
-			var g = map(spectrum[i], 0, 255, 255, 0);
+			let g = map(spectrum[i], 0, 255, 255, 0);
 			fill(spectrum[i], g, 0);
 
-			//draw each bin as a rectangle from the left of the screen
-			//across
-			var y = map(i, 0, spectrum.length, 0, height);
-			var w = map(spectrum[i], 0, 255, 0, width);
-			rect(0, y, w, height / spectrum.length);
+			let x = map(i, 0, spectrum.length, width / 2, width);
+			let y = height / 2;
+			let h = map(spectrum[i], 0, 255, 0, height / 5);
+			let bar_width = width / spectrum.length;
+
+			if (h == 0) continue;
+
+			// Right - top, bottom
+			rect(x, y, bar_width, -h);
+			rect(x, y, bar_width, h);
+			// Left - top, bottom>>
+			rect(width - x, y, bar_width, -h);
+			rect(width - x, y, bar_width, h);
 		}
 		pop();
+
 	};
 }
