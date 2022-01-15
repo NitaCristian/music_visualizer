e6931a5f0c294df3a98b41ada7fea6d21df94adf
Cristian Nita <cristiannita24@gmail.com>
2022-01-15 14:14:41 +0200

    New File Structure
    Changed the original file structure to keep the files more organized.

--- src/visuals/visualisations.js
+++ src/visuals/visualisations.js
@@ -1,5 +1,5 @@
 //container function for the visualisations
-function Visualisations(){
+function Visualisations() {
 	//array to store visualisations
 	this.visuals = [];
 	//currently selected vis. set to null until vis loaded in
@@ -7,20 +7,20 @@ function Visualisations(){
 
 	//add a new visualisation to the array
 	//@param vis: a visualisation object
-	this.add = function(vis){
+	this.add = function (vis) {
 		this.visuals.push(vis);
 		//if selectedVisual is null set the new visual as the 
 		//current visualiation
-		if(this.selectedVisual == null){
+		if (this.selectedVisual == null) {
 			this.selectVisual(vis.name);
 		}
 	};
 
 	//select a visualisation using it name property
 	//@param visName: name property of the visualisation
-	this.selectVisual = function(visName){
-		for(var i = 0; i < this.visuals.length; i++){
-			if(visName == this.visuals[i].name){
+	this.selectVisual = function (visName) {
+		for (var i = 0; i < this.visuals.length; i++) {
+			if (visName == this.visuals[i].name) {
 				this.selectedVisual = this.visuals[i];
 			}
 		}
