c246a9c0c1d9aae1e3cfdaf46ad550c813fa791c
Cristian Nita <cristiannita24@gmail.com>
2022-02-19 13:24:16 +0200

    change the spectrum to class and added comments

--- src/visuals/spectrum.js
+++ src/visuals/spectrum.js
@@ -1,8 +1,16 @@
-function Spectrum() {
-	this.name = "Spectrum";
-	this.rain = new Rain();
 
-	this.draw = function () {
+/** 
+ * @desc Spectrum Visualisation
+ */
+class Spectrum {
+	constructor() {
+		/** @var {String} name Name of the visualisation*/
+		this.name = "Spectrum";
+		/** @var {Rain} rain Rain object which renders droples falling on the screen*/
+		this.rain = new Rain();
+	}
+	draw() {
+		// Draw the rain on the screen
 		this.rain.draw();
 
 		push();
@@ -13,16 +21,22 @@ function Spectrum() {
 			let g = map(spectrum[i], 0, 255, 255, 0);
 			fill(spectrum[i], g, 0);
 
+			// x position of each rectangle, starts in the middle of the canvas
 			let x = map(i, 0, spectrum.length, width / 2, width);
+			// y position of each rectangle, always in the middle of the canvas
 			let y = height / 2;
+			// h height of each rectangle based on the amplitude
 			let h = map(spectrum[i], 0, 255, 0, height / 5) + 1;
+			// bar_width width of each rectangle
 			let bar_width = width / spectrum.length;
 
-			// Right
+			// Draw the rectangle on the right side
+			// Start at the height of the amplitude and 
+			// draw it 2 * its size so its symmetric with respect to the x-axis 
 			rect(x, y - h, bar_width, 2 * h);
-			// Left 
+			// Draw the rectangle on the left side, same as the right side
 			rect(width - x, y - h, bar_width, 2 * h);
 		}
 		pop();
-	};
+	}
 }
--- src/visuals/visualisations.js
+++ src/visuals/visualisations.js
@@ -1,31 +1,3 @@
-// //container function for the visualisations
-// function Visualisations() {
-// 	//array to store visualisations
-// 	this.visuals = [];
-// 	//currently selected vis. set to null until vis loaded in
-// 	this.selectedVisual = null;
-
-// 	//add a new visualisation to the array
-// 	//@param vis: a visualisation object
-// 	this.add = function (vis) {
-// 		this.visuals.push(vis);
-// 		//if selectedVisual is null set the new visual as the 
-// 		//current visualiation
-// 		if (this.selectedVisual == null) {
-// 			this.selectVisual(vis.name);
-// 		}
-// 	};
-
-// 	//select a visualisation using it name property
-// 	//@param visName: name property of the visualisation
-// 	this.selectVisual = function (visName) {
-// 		for (var i = 0; i < this.visuals.length; i++) {
-// 			if (visName == this.visuals[i].name) {
-// 				this.selectedVisual = this.visuals[i];
-// 			}
-// 		}
-// 	};
-// }
 
 /** 
  * @desc Container function for the visualisations
@@ -51,7 +23,7 @@ class Visualisations {
 		if (this.selectedVisual == null) {
 			this.selectVisual(vis.name);
 		}
-	};
+	}
 	/** 
 	 * @desc Select a visualisation using its name property
 	 * 
@@ -63,5 +35,5 @@ class Visualisations {
 				this.selectedVisual = this.visuals[i];
 			}
 		}
-	};
+	}
 }
